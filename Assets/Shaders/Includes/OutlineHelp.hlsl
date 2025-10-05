69b6f40e27b6f9404d7aba58543a19fddb571b75
cereaIx <alicepopilliu@gmail.com>
2025-10-05 16:27:30 -0400

    added basic outline

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -10,3 +10,13 @@ void GetNormal_float(float2 uv, out float3 Normal)
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
+
+void GetCrossSampleUVs_float(float2 uv, float2 TexelSize, float OffsetMultiplier, 
+    out float2 UVTopRight, out float2 UVBottomLeft, out float2 UVTopLeft, 
+    out float2 UVBottomRight)
+{
+    UVTopRight = uv + float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomLeft = uv - float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVTopLeft = uv + float2(-TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomRight = uv + float2(TexelSize.x, -TexelSize.y) * OffsetMultiplier;
+}
